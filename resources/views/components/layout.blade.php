45e15446714d695f2097467e388044c0ec710f3a
Tunde James <tundejames04@gmail.com>
2024-06-06 17:36:03 +0100

    Create a layout file using blade components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,16 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact Page</title>
+  <title>Home Page</title>
 </head>
 
 <body>
-  <h1>Hello, from the contact page</h1>
-</body>
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+  </nav>
 
+  {{ $slot }}    
+</body>
 </html>
