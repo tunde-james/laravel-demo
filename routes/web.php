574d4e295ab63963d155249e5c7f7a0e3d59d666
Tunde James <tundejames04@gmail.com>
2024-06-11 12:15:20 +0100

    Refactor: extract the route closures into a contoller and group them into a route resource.

--- routes/web.php
+++ routes/web.php
@@ -1,86 +1,9 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::get('/', function () {
-  return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-// index
-Route::get('/jobs', function () {
-  $jobs = Job::with('employer')->latest()->simplePaginate(3);
-
-  return view('jobs.index', [
-    'jobs' => $jobs
-  ]);
-});
-
-// create
-Route::get('jobs/create', function () {
-  return view('jobs.create');
-});
-
-// show
-Route::get('/jobs/{id}', function ($id) {
-  $job = Job::find($id);
-
-  return view('jobs.show', ['job' => $job]);
-});
-
-// store
-Route::post('/jobs', function () {
-  request()->validate([
-    'title' => ['required', 'min:3'],
-    'salary' => ['required']
-  ]);
-
-  Job::create([
-    'title' => request('title'),
-    'salary' => request('salary'),
-    'employer_id' => 1
-  ]);
-
-  return redirect('/jobs');
-});
-
-// edit
-Route::get('/jobs/{id}/edit', function ($id) {
-  $job = Job::find($id);
-
-  return view('jobs.edit', ['job' => $job]);
-});
-
-// update
-Route::patch('/jobs/{id}', function ($id) {
-  // validate
-  request()->validate([
-    'title' => ['required', 'min:3'],
-    'salary' => ['required']
-  ]);
-
-  // authorize (On hold...)
-
-  $job = Job::findorFail($id);
-
-  $job->update([
-    'title' => request('title'),
-    'salary' => request('salary')
-  ]);
-
-  return redirect('/jobs/' . $job->id);
-});
-
-// delete/destroy
-Route::delete('/jobs/{id}', function ($id) {
-  // authorize the request (on hold...)
-
-  // delete the job
-  Job::findorFail($id)->delete();
-
-  return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-  return view('contact');
-});
+Route::resource('job', JobController::class);
